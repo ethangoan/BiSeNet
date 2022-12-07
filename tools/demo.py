e8ed3d9b1fc69c60beebcb5e58a64dd22ff0096f
Ethan Goan <ethanjgoan@gmail.com>
2022-12-07 13:49:12 +1000

    dev: updated demo script

--- tools/demo.py
+++ tools/demo.py
@@ -129,36 +129,6 @@ torch.set_grad_enabled(False)
 np.random.seed(123)
 
 
-class ADFSoftmax(nn.Module):
-    def __init__(self, dim=1, keep_variance_fn=None):
-        super(ADFSoftmax, self).__init__()
-        self.dim = dim
-        self._keep_variance_fn = keep_variance_fn
-
-    def forward(self, features_mean, features_variance, eps=1e-5):
-        """Softmax function applied to a multivariate Gaussian distribution.
-        It works under the assumption that features_mean and features_variance
-        are the parameters of a the indepent gaussians that contribute to the
-        multivariate gaussian.
-        Mean and variance of the log-normal distribution are computed following
-        https://en.wikipedia.org/wiki/Log-normal_distribution."""
-        log_gaussian_mean = features_mean + 0.5 * features_variance
-        log_gaussian_variance = 2 * log_gaussian_mean
-
-        log_gaussian_mean = torch.exp(log_gaussian_mean)
-        log_gaussian_variance = torch.exp(log_gaussian_variance)
-        log_gaussian_variance = log_gaussian_variance*(torch.exp(features_variance)-1)
-
-        constant = torch.sum(log_gaussian_mean, dim=self.dim) + eps
-        constant = constant.unsqueeze(self.dim)
-        outputs_mean = log_gaussian_mean/constant
-        outputs_variance = log_gaussian_variance/(constant**2)
-
-        if self._keep_variance_fn is not None:
-            outputs_variance = self._keep_variance_fn(outputs_variance)
-        return outputs_mean, outputs_variance
-
-
 
 
 # following fns and color maps taken from CSVAIL
